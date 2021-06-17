3e5e7a9d96bd2f11812153ecb5da91e0344c5045
Yan Shlyahov <godyanuss@gmail.com>
2021-06-17 20:56:49 +0300

    Initial commit

---
+++ index.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+from . import settings
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello world'
+
+
+if __name__ == '__main__':
+    app.run(
+        host=settings.HOST,
+        port=settings.PORT,
+        debug=settings.DEBUG,
+    )
